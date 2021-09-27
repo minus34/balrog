9eb8cbe670dc5fbe4bf48456077ee073003e668a
Hugh Saalmans <hugh.saalmans@gmail.com>
2021-09-27 21:07:15 +1000

    added processing a folder

--- testing/test_gdal.py
+++ testing/test_gdal.py
@@ -1,28 +1,45 @@
 
+import glob
+import os
+# import numpy as np
+# import rasterio
+
 from osgeo import gdal
-import numpy as np
-import rasterio
 
-dem_file_path = "/Users/s57405/Downloads/NSW Government - Spatial Services/DEM/1 Metre/PortHacking202004-LID1-AHD_3186234_56_0002_0002_1m/PortHacking202004-LID1-AHD_3186234_56_0002_0002_1m.asc"
+# the directory of this script
+script_dir = os.path.dirname(os.path.realpath(__file__))
 
-
-def calculate_slope(DEM):
-    gdal.DEMProcessing('slope.tif', DEM, 'slope')
-    with rasterio.open('slope.tif') as dataset:
-        slope=dataset.read(1)
-    return slope
+# input dem files path
+input_path = os.path.join(script_dir, "input", "*.asc")
+output_path = os.path.join(script_dir, "output")
 
 
-def calculate_aspect(DEM):
-    gdal.DEMProcessing('aspect.tif', DEM, 'aspect')
-    with rasterio.open('aspect.tif') as dataset:
-        aspect=dataset.read(1)
-    return aspect
+def process_dem(dem_file, output_type):
+    output_file = os.path.join(output_path, output_type,
+                               os.path.basename(file_path).replace(".asc", f"_{output_type}.tif"))
+
+    if output_type == "color-relief":
+        gdal.DEMProcessing(output_file, dem_file, output_type,
+                           colorFilename=os.path.join(script_dir, "colour_palette.txt"))
+    else:
+        gdal.DEMProcessing(output_file, dem_file, output_type)
+
+    # with rasterio.open(f"_{output_type}") as dataset:
+    #     slope=dataset.read(1)
+    # return slope
 
 
-slope=calculate_slope(dem_file_path)
-aspect=calculate_aspect(dem_file_path)
+for file_path in glob.glob(input_path):
+    # process_dem(file_path, "hillshade")
+    # process_dem(file_path, "color-relief")
+    process_dem(file_path, "slope")
+    process_dem(file_path, "aspect")
 
-print(type(slope))
-print(slope.dtype)
-print(slope.shape)
+    print(f"Processed : {os.path.basename(file_path)}")
+
+#     slope = calculate_slope(dem_file_path)
+#     aspect = calculate_aspect(dem_file_path)
+
+# print(type(slope))
+# print(slope.dtype)
+# print(slope.shape)
