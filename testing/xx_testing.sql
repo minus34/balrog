e59ca19802c2929f058afe9bc3434605f4ad952d
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-10-08 16:12:06 +1100

    Dancing around a PostGIS bug

--- testing/xx_testing.sql
+++ testing/xx_testing.sql
@@ -93,36 +93,46 @@ analyse bushfire.bal_factors_test_sydney;
 
 
 -- create Geoscape building buffer table
+
+-- convert to polygons (they aren't multipolygons!) -- 15,841,377 rows affected in 4 m 35 s 202 ms
 drop table if exists bushfire.temp_buildings;
 create table bushfire.temp_buildings as
+    with bld as (
+        select bld_pid,
+               (st_dump(geom)).geom as geom
+        from geo_propertyloc.aus_buildings_polygons
+    )
 select bld_pid,
-       (st_dump(geom)).geom as geom
-from geo_propertyloc.aus_buildings_polygons
+       geom,
+       geom::geography as geog,
+       st_transform(geom, 28356) as geom_mga56
+    from bld
 ;
 analyse bushfire.temp_buildings;
 
+-- WGA84 lat/long buildings with a 100m buffer
 drop table if exists bushfire.buildings;
 create table bushfire.buildings as
 select bld_pid,
-       st_asgeojson(st_transform(geom, 4326), 6)::jsonb as geom,
-       st_asgeojson(st_transform(st_buffer(geom::geography, 100)::geometry, 4326), 6)::jsonb as buffer
+       st_asgeojson(geom, 6, 0)::jsonb as geom,
+       st_asgeojson(st_buffer(geog, 100, 8), 6, 0)::jsonb as buffer
 from bushfire.temp_buildings
 ;
 analyse bushfire.buildings;
 
--- create Geoscape building buffer table with MGA 56 geometries
+-- MGA Zone 56 buildings with a 100m buffer
 drop table if exists bushfire.buildings_mga56;
 create table bushfire.buildings_mga56 as
 select bld_pid,
-       st_asgeojson(st_transform(geom, 28356), 1)::jsonb as geom,
-       st_asgeojson(st_buffer(st_transform(geom, 28356), 100.0), 1)::jsonb as buffer
+       st_asgeojson(geom_mga56, 1, 0)::jsonb as geom,
+       st_asgeojson(st_buffer(geom_mga56, 100.0, 8), 1, 0)::jsonb as buffer
 from bushfire.temp_buildings
 ;
 analyse bushfire.buildings_mga56;
 
 
 
-
+select count(*) from bushfire.temp_buildings;
 
 
 
