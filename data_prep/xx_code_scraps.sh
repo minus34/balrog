02e3b9ddcf1cc9cf5df023bffdaed9b0c6184003
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-10-12 18:38:31 +1100

    it works

--- data_prep/xx_code_scraps.sh
+++ data_prep/xx_code_scraps.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
 
-/Applications/Postgres.app/Contents/Versions/13/bin/pg_restore -Fc -d geo -p 5432 -U postgres /Users/s57405/Downloads/buildings_json.dmp
+# remote dump of results
+pg_dump -Fc -d geo -t bushfire.bal_factors -p 5432 -U "ec2-user" -f "/data/bal_factors.dmp" --no-owner
+aws s3 cp /data/bal_factors.dmp s3://bushfire-rasters/output/
+
+# local
+aws s3 cp s3://bushfire-rasters/output/bal_factors.dmp /Users/s57405/Downloads/
+psql -d geo -c "drop table bushfire.bal_factors"
+/Applications/Postgres.app/Contents/Versions/13/bin/pg_restore -Fc -d geo -p 5432 -U postgres /Users/s57405/Downloads/bal_factors.dmp
+
+
+
+
 
 
 # nope!
