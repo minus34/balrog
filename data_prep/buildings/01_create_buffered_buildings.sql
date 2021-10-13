690cac8c5fe73c1f7784b6e471c6e0a1444b1b6b
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-10-13 19:46:21 +1100

    restructuring files & directories

--- data_prep/buildings/01_create_buffered_buildings.sql
+++ data_prep/buildings/01_create_buffered_buildings.sql
@@ -2,7 +2,7 @@
 -- Requires a licensed copy of Geoscape's Buildings dataset: https://geoscape.com.au/data/buildings/
 --
 
--- make 100m buffers out of 2D building outlines - these will be used to get aspect & slope -- 46 mins
+-- make 100m buffers out of 2D building outlines - these will be used to get aspect & slope -- 45 mins
 drop table if exists bushfire.temp_building_buffers;
 create table bushfire.temp_building_buffers as
 select bld_pid,
