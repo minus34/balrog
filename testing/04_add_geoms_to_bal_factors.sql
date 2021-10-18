1d65f8c9a4b39009cebb45a15bc4de375347df09
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-10-18 14:02:31 +1100

    renamed columns correctly

--- testing/04_add_geoms_to_bal_factors.sql
+++ testing/04_add_geoms_to_bal_factors.sql
@@ -2,12 +2,27 @@
 -- add geoms to bal factor results
 drop table if exists bushfire.bal_factors_srtm_1sec;
 create table bushfire.bal_factors_srtm_1sec as -- 15,839,641 rows affected in 3 m 18 s 964 ms
-select bal.*,
+select geo.bld_pid,
+       aspect_min,
+       aspect_max,
+       aspect_avg,
+       aspect_std,
+       aspect_med,
+       slope_min,
+       slope_max,
+       slope_avg,
+       slope_std,
+       slope_med,
+       dem_min,
+       dem_max,
+       dem_avg,
+       dem_std,
+       dem_med,
        geo.grd_elev,
        geo.grd_el_src,
        geo.geom
 from bushfire.bal_factors as bal
-         inner join geo_propertyloc.aus_buildings_polygons as geo on bal.bld_pid = geo.bld_pid
+         inner join geo_propertyloc.aus_buildings_polygons as geo on bal.id = geo.bld_pid
 ;
 analyse bushfire.bal_factors_srtm_1sec;
 
