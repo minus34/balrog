0468d07e07dcbc38c1404608bfc7656611b5058b
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-10-21 18:26:47 +1100

    testing flatgeobuf vegetation

--- data_prep/vegetation/xx_fiona_testing.py
+++ data_prep/vegetation/xx_fiona_testing.py
@@ -9,5 +9,9 @@ start_time = datetime.now()
 
 with fiona.open("s3://bushfire-rasters/vegetation/nvis6_bal.fgb") as src:
     print(len(src))  # should be 1931443
+    f = next(iter(src))
+    print(f['geometry']['type'])
+    print(f['properties'])
 
 print(f"it took {datetime.now() - start_time}")
+
