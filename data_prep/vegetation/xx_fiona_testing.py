e7b31c5a29231953bb3664f4f0012137cf7b61e6
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-10-21 18:34:01 +1100

    testing flatgeobuf vegetation

--- data_prep/vegetation/xx_fiona_testing.py
+++ data_prep/vegetation/xx_fiona_testing.py
@@ -1,9 +1,7 @@
 
-
 import fiona
 
 from datetime import datetime
-
 
 start_time = datetime.now()
 
@@ -12,11 +10,8 @@ with fiona.open("s3://bushfire-rasters/vegetation/nvis6_bal.fgb") as src:
 
     for f in src.filter(bbox=(149, -34.0, 149.1, -33.9)):
         poly = iter(f)
-        # print(f['geometry']['type'])
+        print(f['geometry']['type'])
         print(f['properties'])
 
 print(f"it took {datetime.now() - start_time}")
-
-
-
 
