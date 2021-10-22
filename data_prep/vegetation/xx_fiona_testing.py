492904c3a846c4a1dcfdfb3b0e69509389c872cd
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-10-22 12:17:07 +1100

    mask data using buffer working

--- data_prep/vegetation/xx_fiona_testing.py
+++ data_prep/vegetation/xx_fiona_testing.py
@@ -23,7 +23,7 @@ project_2_wgs84 = pyproj.Transformer.from_crs(lcc, wgs84, always_xy=True).transf
 # create input buffer polygon as both a WGS84 shape and a dict
 wgs84_point = Point(longitude, latitude)
 lcc_point = transform(project_2_lcc, wgs84_point)
-buffer = transform(project_2_wgs84, lcc_point.buffer(200, cap_style=1))
+buffer = transform(project_2_wgs84, lcc_point.buffer(250, cap_style=1))
 dict_buffer = mapping(buffer)  # a dict representing a GeoJSON geometry
 # print(str(buffer))
 # print(mapping(buffer))
@@ -32,19 +32,22 @@ print(f"created buffer : {datetime.now() - start_time}")
 start_time = datetime.now()
 
 # open file and filter by buffer
-with fiona.open("s3://bushfire-rasters/vegetation/nvis6_bal.fgb") as src:
+with fiona.open("/Users/s57405/tmp/bushfire/nvis6_bal.fgb") as src:
+# with fiona.open("s3://bushfire-rasters/vegetation/nvis6_bal.fgb") as src:
     # print(len(src))  # should be 3798424
-
     row_count = 0
+    clipped_list = list()
 
     for f in src.filter(mask=dict_buffer):
-    # for f in src.filter(bbox=(150.387354,-33.730476, 150.401037,-33.720566)):
-        poly = iter(f)
+        props_dict = dict()
+        geom_dict = dict()
+
+        # poly = iter(f)
         # print(f['geometry']['type'])
         # print(f['properties'])
 
         props = f['properties']
-        geom = Polygon(f['geometry']['coordinates'][0])
+        geom = Polygon(f['geometry']['coordinates'][0]).intersection(buffer)
 
         print(f"{geom.type} : {props['bal_name']} : {props['area_m2']} m2")
 
