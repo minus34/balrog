db2a0d2c4567e044b7dbfc70c85f412b90cedf3f
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-10-25 16:01:10 +1100

    added s3 copy

--- data_prep/vegetation/geoscape/xx_create_gdal_merge_command.py
+++ data_prep/vegetation/geoscape/xx_create_gdal_merge_command.py
@@ -10,11 +10,15 @@ output_file = os.path.join(pathlib.Path.home(), "tmp/bushfire/veg/geoscape_trees
 file_list = [os.path.basename(file_name) for file_name in glob.glob(os.path.join(input_path, "*.tif"))]
 files_string = " ".join(file_list)
 
-command = f"gdal_merge.py -o {output_file} -of COG -co BIGTIFF=YES -co COMPRESS=DEFLATE -co NUM_THREADS=ALL_CPUS {files_string}"
+command1 = f"gdal_merge.py -o temp.tif -of GTiff -co BIGTIFF=YES -co COMPRESS=DEFLATE -co NUM_THREADS=ALL_CPUS {files_string}\n"
+command2 = f"gdal_translate.py temp.tif {output_file} -of COG -co BIGTIFF=YES -co COMPRESS=DEFLATE -co NUM_THREADS=ALL_CPUS\n"
 
 # out commands to bash file
 with open("xx_trees_merge_images.sh", "w") as f:
     f.write("#!/usr/bin/env bash\n\n")
     f.write("conda activate geo\n\n")
     f.write(f"cd '{input_path}'\n\n")
-    f.write(command)
+    f.write(command1)
+    f.write(command2)
+    f.write("rm temp.tif")
+    f.write(f"aws s3 cp {output_file} s3://bushfire-rasters/geoscape/")
