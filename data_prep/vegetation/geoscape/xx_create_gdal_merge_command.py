01f3b92a67046a4ed8708f955975aa35f9b27785
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-10-26 10:14:34 +1100

    rewritten to run entirely within Python

--- data_prep/vegetation/geoscape/xx_create_gdal_merge_command.py
+++ data_prep/vegetation/geoscape/xx_create_gdal_merge_command.py
@@ -8,23 +8,23 @@ from boto3.s3.transfer import TransferConfig
 from datetime import datetime
 from osgeo import gdal
 
+full_start_time = datetime.now()
+print(f"START - mosaic and transform images : {full_start_time}")
+
 # setup connection to AWS S3
 s3_client = boto3.client("s3")
 s3_config = TransferConfig(multipart_threshold=10240 ** 2)  # 10MB
 
 s3_bucket = "bushfire-rasters"
-s3_file_path = "geoscpae/geoscape_2m_land_cover.tif"
-
+s3_file_path = "geoscape/geoscape_2m_land_cover.tif"
 
 input_path = os.path.join(pathlib.Path.home(), "Downloads/SurfaceCover_JUN21_ALLSTATES_GDA94_GEOTIFF_161/Surface Cover/Surface Cover 2M JUNE 2021/Standard")
 output_file = os.path.join(pathlib.Path.home(), "tmp/bushfire/veg/geoscape_2m_land_cover.tif")
 
 warped_files = list()
 
-full_start_time = datetime.now()
-
 # mosaic and transform to WGS84 lat/long for each MGA zone (aka UTM South zones on GDA94 datum)
-for zone in range(49, 50):
+for zone in range(49, 57):
     start_time = datetime.now()
 
     print(f"Processing MGA Zone {zone}")
@@ -35,26 +35,28 @@ for zone in range(49, 50):
 
     my_vrt = gdal.BuildVRT(vrt_file, files_to_mosaic)
     my_vrt = None
-    print(f"\t - VRT created : {datetime.now() - start_time}")
-    start_time = datetime.now()
+    # print(f"\t - VRT created : {datetime.now() - start_time}")
+    # start_time = datetime.now()
 
     gd = gdal.Warp(interim_file, vrt_file, format="GTiff", options="-t_srs EPSG:4326 -co BIGTIFF=YES -co COMPRESS=DEFLATE -co NUM_THREADS=ALL_CPUS -overwrite")
     del gd
+    os.remove(vrt_file)
     print(f"\t - mosaiced and transformed images : {datetime.now() - start_time}")
 
     warped_files.append(interim_file)
 
+# mosaic all merged files and output as a single Cloud Optimised GeoTIFF (COG) for all of AU
 start_time = datetime.now()
-
 print(f"Processing AU")
 vrt_file = os.path.join(input_path, "temp_au.vrt")
 my_vrt = gdal.BuildVRT(os.path.join(input_path, "temp_au.vrt"), warped_files)
 my_vrt = None
-print(f"\t - VRT created : {datetime.now() - start_time}")
-start_time = datetime.now()
+# print(f"\t - VRT created : {datetime.now() - start_time}")
+# start_time = datetime.now()
 
 gd = gdal.Warp(output_file, vrt_file, format="COG", options="-co BIGTIFF=YES -co COMPRESS=DEFLATE -co NUM_THREADS=ALL_CPUS -overwrite")
 del gd
+os.remove(vrt_file)
 print(f"\t - mosaiced and transformed images : {datetime.now() - start_time}")
 start_time = datetime.now()
 
@@ -63,6 +65,7 @@ for file in warped_files:
     os.remove(file)
 
 # upload to AWS S3
-aws_response = s3_client.upload_fileobj(output_file, s3_bucket, s3_file_path, Config=s3_config)
+aws_response = s3_client.upload_file(output_file, s3_bucket, s3_file_path, Config=s3_config)
 print(f"\t - image uploaded to s3 : {datetime.now() - start_time}")
-start_time = datetime.now()
+
+print(f"FINISHED - mosaic and transform images : {datetime.now() - full_start_time}")
