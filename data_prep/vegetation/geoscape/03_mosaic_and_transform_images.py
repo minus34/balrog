a5c9a3cb2532aef7815681a6f8e63aab6dc727ed
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-10-26 14:58:49 +1100

    testing no vrt file

--- data_prep/vegetation/geoscape/03_mosaic_and_transform_images.py
+++ data_prep/vegetation/geoscape/03_mosaic_and_transform_images.py
@@ -2,6 +2,7 @@
 import boto3
 import glob
 import os
+import platform
 
 from boto3.s3.transfer import TransferConfig
 from datetime import datetime
@@ -13,19 +14,32 @@ s3_config = TransferConfig(multipart_threshold=10240 ** 2)  # 20MB
 
 s3_bucket = "bushfire-rasters"
 
-input_list = [{"name": "2m land cover",
-           "input_path": "/data/geoscape/Surface Cover/Surface Cover 2M JUNE 2021/Standard",
-           "output_file": "/data/geoscape/geoscape_2m_land_cover.tif",
-           "s3_file_path": "geoscape/geoscape_2m_land_cover.tif"},
-          {"name": "30m land cover",
-           "input_path": "/data/geoscape/Surface Cover/Surface Cover 30M JUNE 2021/Standard",
-           "output_file": "/data/geoscape/geoscape_30m_land_cover.tif",
-           "s3_file_path": "geoscape/geoscape_30m_land_cover.tif"},
-          {"name": "trees",
-           "input_path": "/data/geoscape/Trees/Trees JUNE 2021/Standard",
-           "output_file": "/data/geoscape/geoscape_trees.tif",
-           "s3_file_path": "geoscape/geoscape_trees.tif"}
-]
+if platform.system() == "Darwin":
+    input_list = [{"name": "2m land cover",
+               "input_path": "/Users/s57405/Downloads/SurfaceCover_JUN21_ALLSTATES_GDA94_GEOTIFF_161/Surface Cover/Surface Cover 2M JUNE 2021/Standard",
+               "output_file": "/Users/s57405/tmp/bushfire/veg/geoscape_2m_land_cover.tif",
+               "s3_file_path": "geoscape/geoscape_2m_land_cover.tif"},
+              {"name": "30m land cover",
+               "input_path": "/Users/s57405/Downloads/SurfaceCover_JUN21_ALLSTATES_GDA94_GEOTIFF_161/Surface Cover/Surface Cover 30M JUNE 2021/Standard",
+               "output_file": "/Users/s57405/tmp/bushfire/veg/geoscape_30m_land_cover.tif",
+               "s3_file_path": "geoscape/geoscape_30m_land_cover.tif"},
+              {"name": "trees",
+               "input_path": "/Users/s57405/Downloads/Trees_JUN21_ALLSTATES_GDA94_GEOTIFF_161/Trees/Trees JUNE 2021/Standard",
+               "output_file": "/Users/s57405/tmp/bushfire/veg/geoscape_trees.tif",
+               "s3_file_path": "geoscape/geoscape_trees.tif"}]
+else:
+    input_list = [{"name": "2m land cover",
+                   "input_path": "/data/geoscape/Surface Cover/Surface Cover 2M JUNE 2021/Standard",
+                   "output_file": "/data/geoscape/geoscape_2m_land_cover.tif",
+                   "s3_file_path": "geoscape/geoscape_2m_land_cover.tif"},
+                  {"name": "30m land cover",
+                   "input_path": "/data/geoscape/Surface Cover/Surface Cover 30M JUNE 2021/Standard",
+                   "output_file": "/data/geoscape/geoscape_30m_land_cover.tif",
+                   "s3_file_path": "geoscape/geoscape_30m_land_cover.tif"},
+                  {"name": "trees",
+                   "input_path": "/data/geoscape/Trees/Trees JUNE 2021/Standard",
+                   "output_file": "/data/geoscape/geoscape_trees.tif",
+                   "s3_file_path": "geoscape/geoscape_trees.tif"}]
 
 # process 1 dataset at a time using parallel processing (built into GDAL)
 for input_dict in input_list:
@@ -41,15 +55,15 @@ for input_dict in input_list:
         print(f" - {input_dict['name']} - Processing MGA Zone {zone}")
 
         files_to_mosaic = glob.glob(os.path.join(input_dict["input_path"], f"*_Z{zone}_*.tif"))
-        vrt_file = os.path.join(input_dict["input_path"], f"temp_Z{zone}.vrt")
+        # vrt_file = os.path.join(input_dict["input_path"], f"temp_Z{zone}.vrt")
         interim_file = os.path.join(input_dict["input_path"], f"temp_Z{zone}.tif")
 
-        my_vrt = gdal.BuildVRT(vrt_file, files_to_mosaic)
-        my_vrt = None
+        # my_vrt = gdal.BuildVRT(vrt_file, files_to_mosaic)
+        # my_vrt = None
 
-        gd = gdal.Warp(interim_file, vrt_file, format="GTiff", options="-multi -wm 80% -t_srs EPSG:4326 -co BIGTIFF=YES -co COMPRESS=DEFLATE -co NUM_THREADS=ALL_CPUS -overwrite")
+        gd = gdal.Warp(interim_file, files_to_mosaic, format="GTiff", options="-multi -wm 80% -t_srs EPSG:4326 -co BIGTIFF=YES -co COMPRESS=DEFLATE -co NUM_THREADS=ALL_CPUS -overwrite")
         del gd
-        os.remove(vrt_file)
+        # os.remove(vrt_file)
 
         warped_files.append(interim_file)
 
@@ -59,13 +73,13 @@ for input_dict in input_list:
     start_time = datetime.now()
     print(f" - {input_dict['name']} - Processing AU")
 
-    vrt_file = os.path.join(input_dict["input_path"], "temp_au.vrt")
-    my_vrt = gdal.BuildVRT(os.path.join(input_dict["input_path"], "temp_au.vrt"), warped_files)
-    my_vrt = None
+    # vrt_file = os.path.join(input_dict["input_path"], "temp_au.vrt")
+    # my_vrt = gdal.BuildVRT(os.path.join(input_dict["input_path"], "temp_au.vrt"), warped_files)
+    # my_vrt = None
 
-    gd = gdal.Warp(input_dict["output_file"], vrt_file, format="COG", options="-multi -wm 80% -co BIGTIFF=YES -co COMPRESS=DEFLATE -co NUM_THREADS=ALL_CPUS -overwrite")
+    gd = gdal.Warp(input_dict["output_file"], warped_files, format="COG", options="-multi -wm 80% -co BIGTIFF=YES -co COMPRESS=DEFLATE -co NUM_THREADS=ALL_CPUS -overwrite")
     del gd
-    os.remove(vrt_file)
+    # os.remove(vrt_file)
 
     print(f"\t - {input_dict['name']} - mosaiced and transformed images : {datetime.now() - start_time}")
     start_time = datetime.now()
