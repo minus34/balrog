fb56b73fb6d94cc0bbae0bf7117ce6639cdeb7b6
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-11-05 15:27:03 +1100

    making data valid & splitting for faster querying

--- data_prep/vegetation/nsw_rfs/xx_import_bfpl.sh
+++ data_prep/vegetation/nsw_rfs/xx_import_bfpl.sh
@@ -9,6 +9,18 @@ ogr2ogr -overwrite -f "PostgreSQL" PG:"${PG_CONNECT_STRING}" "/Users/$(whoami)/D
 
 
 
+#Warning 1: organizePolygons() received an unexpected geometry.  Either a polygon with interior rings, or a polygon with less than 4 points, or a non-Polygon geometry.  Return arguments as a collection.
+#Warning 1: Geometry of polygon of fid 226388 cannot be translated to Simple Geometry. All polygons will be contained in a multipolygon.
+#Warning 1: organizePolygons() received an unexpected geometry.  Either a polygon with interior rings, or a polygon with less than 4 points, or a non-Polygon geometry.  Return arguments as a collection.
+#Warning 1: Geometry of polygon of fid 226785 cannot be translated to Simple Geometry. All polygons will be contained in a multipolygon.
+#Warning 1: organizePolygons() received an unexpected geometry.  Either a polygon with interior rings, or a polygon with less than 4 points, or a non-Polygon geometry.  Return arguments as a collection.
+#Warning 1: Geometry of polygon of fid 694967 cannot be translated to Simple Geometry. All polygons will be contained in a multipolygon.
+#Warning 1: organizePolygons() received an unexpected geometry.  Either a polygon with interior rings, or a polygon with less than 4 points, or a non-Polygon geometry.  Return arguments as a collection.
+#Warning 1: Geometry of polygon of fid 695003 cannot be translated to Simple Geometry. All polygons will be contained in a multipolygon.
+
+
+
+
 
 
 #aws s3 sync /Users/$(whoami)/Downloads/nvis6/ s3://bushfire-rasters/vegetation/nvis6/ --exclude .DS_Store
--- data_prep/vegetation/nsw_rfs/xx_testing.sql
+++ data_prep/vegetation/nsw_rfs/xx_testing.sql
@@ -22,31 +22,46 @@
 -- alter table bushfire.nsw_rfs_bushfire_prone_land owner to postgres;
 
 
-
-
--- geometry types
-select st_geometrytype(geom) as geom_type,
-       sum(st_numgeometries(geom)) as geom_count,
-       count(*) as row_count
-from bushfire.nsw_rfs_bushfire_prone_land
-group by geom_type
+-- STEP 1 -- create a table of exploded, valid polygons
+drop table if exists bushfire.nsw_rfs_bushfire_prone_land_exploded;
+create table bushfire.nsw_rfs_bushfire_prone_land_exploded as
+with bf as (
+    select category::smallint as category,
+           d_category,
+           (st_dump(st_force2d(geom))).geom as geom
+    from bushfire.nsw_rfs_bushfire_prone_land
+)
+select row_number() over () as gid,
+       category,
+       d_category,
+       st_npoints(geom) as point_count,
+       st_isvalid(geom) as is_geom_valid,
+       st_makevalid(geom) as geom
+from bf
 ;
 
+-- only allow this after testing for non-polygon rows
+-- delete from bushfire.nsw_rfs_bushfire_prone_land_exploded
+select * from bushfire.nsw_rfs_bushfire_prone_land_exploded
+where st_geometrytype(geom) <> 'ST_Polygon'
+;
+analyse bushfire.nsw_rfs_bushfire_prone_land_exploded;
 
 
--- subdivide polygons (some of them are very large) to speed up analysis
+-- STEP 2 -- subdivide polygons (some of them are very large) to speed up analysis
 drop table if exists bushfire.nsw_rfs_bushfire_prone_land_analysis;
 create table bushfire.nsw_rfs_bushfire_prone_land_analysis as
-select category,
+select gid,
+       category,
        d_category,
        st_subdivide(st_makevalid(geom), 512) as geom
-from bushfire.nsw_rfs_bushfire_prone_land
-where st_geometrytype(geom) in ('ST_Polygon', 'ST_MultiPolygon')
+from bushfire.nsw_rfs_bushfire_prone_land_exploded
+where st_geometrytype(geom) = 'ST_Polygon'
 ;
 analyse bushfire.nsw_rfs_bushfire_prone_land_analysis;
 
-CREATE INDEX nvis6_bal_analysis_d_category_idx ON bushfire.nsw_rfs_bushfire_prone_land_analysis USING btree (d_category);
-CREATE INDEX nvis6_bal_analysis_category_idx ON bushfire.nsw_rfs_bushfire_prone_land_analysis USING btree (category);
-CREATE INDEX nvis6_bal_analysis_geom_idx ON bushfire.nsw_rfs_bushfire_prone_land_analysis USING gist (geom);
-ALTER TABLE bushfire.nsw_rfs_bushfire_prone_land_analysis CLUSTER ON nvis6_bal_analysis_geom_idx;
+CREATE INDEX nsw_rfs_bushfire_prone_land_analysis_d_category_idx ON bushfire.nsw_rfs_bushfire_prone_land_analysis USING btree (d_category);
+CREATE INDEX nsw_rfs_bushfire_prone_land_analysis_category_idx ON bushfire.nsw_rfs_bushfire_prone_land_analysis USING btree (category);
+CREATE INDEX nsw_rfs_bushfire_prone_land_analysis_geom_idx ON bushfire.nsw_rfs_bushfire_prone_land_analysis USING gist (geom);
+ALTER TABLE bushfire.nsw_rfs_bushfire_prone_land_analysis CLUSTER ON nsw_rfs_bushfire_prone_land_analysis_geom_idx;
 
