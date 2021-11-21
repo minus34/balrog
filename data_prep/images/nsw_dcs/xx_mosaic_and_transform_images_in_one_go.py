c1812b008660845655be8266abb0f9c392e64727
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-11-22 10:40:24 +1100

    optimised process

--- data_prep/images/nsw_dcs/xx_mosaic_and_transform_images_in_one_go.py
+++ data_prep/images/nsw_dcs/xx_mosaic_and_transform_images_in_one_go.py
@@ -19,48 +19,48 @@ s3_config = TransferConfig(multipart_threshold=10240 ** 2)  # 20MB
 
 s3_bucket = "bushfire-rasters"
 
-# base_url = "https://portal.spatial.nsw.gov.au/download/dem"
+base_url = "https://portal.spatial.nsw.gov.au/download/dem"
 
 
 if platform.system() == "Darwin":
-    debug = False
+    debug = True
 
-    urls = ["https://portal.spatial.nsw.gov.au/download/dem/56/Sydney-DEM-AHD_56_5m.zip",
-            "https://portal.spatial.nsw.gov.au/download/dem/56/Wollongong-DEM-AHD_56_5m.zip",
-            "https://portal.spatial.nsw.gov.au/download/dem/56/Penrith-DEM-AHD_56_5m.zip",
-            "https://portal.spatial.nsw.gov.au/download/dem/56/Katoomba-DEM-AHD_56_5m.zip",
-            "https://portal.spatial.nsw.gov.au/download/dem/56/PortHacking-DEM-AHD_56_5m.zip",
-            "https://portal.spatial.nsw.gov.au/download/dem/56/Burragorang-DEM-AHD_56_5m.zip"
-            ]
+    # urls = ["https://portal.spatial.nsw.gov.au/download/dem/56/Sydney-DEM-AHD_56_5m.zip",
+    #         "https://portal.spatial.nsw.gov.au/download/dem/56/Wollongong-DEM-AHD_56_5m.zip",
+    #         "https://portal.spatial.nsw.gov.au/download/dem/56/Penrith-DEM-AHD_56_5m.zip",
+    #         "https://portal.spatial.nsw.gov.au/download/dem/56/Katoomba-DEM-AHD_56_5m.zip",
+    #         "https://portal.spatial.nsw.gov.au/download/dem/56/PortHacking-DEM-AHD_56_5m.zip",
+    #         "https://portal.spatial.nsw.gov.au/download/dem/56/Burragorang-DEM-AHD_56_5m.zip"
+    #         ]
 
     ram_to_use = 8
 
-    input_path = os.path.join(pathlib.Path.home(), "tmp/bushfire/nsw_dcs/nsw_dcs_5m_dem")
-    glob_pattern = "*-DEM-AHD_56_5m.zip"
+    # input_path = os.path.join(pathlib.Path.home(), "tmp/bushfire/nsw_dcs/nsw_dcs_5m_dem")
+    # glob_pattern = "*-DEM-AHD_56_5m.zip"
 
     output_path = os.path.join(pathlib.Path.home(), "tmp/bushfire/nsw_dcs")
     temp_output_path = os.path.join(output_path, "tmp")
 
-    output_dem_file = os.path.join(output_path, "nsw_dcs_5m_dem.tif")
-    output_slope_file = os.path.join(output_path, "nsw_dcs_5m_slope.tif")
+    output_dem_file = os.path.join(output_path, "test_nsw_dcs_5m_dem.tif")
+    output_slope_file = os.path.join(output_path, "test_nsw_dcs_5m_slope.tif")
 
 else:
     debug = False
 
     ram_to_use = 480
 
-    input_path = os.path.join(pathlib.Path.home(), "tmp/bushfire/nsw_dcs/nsw_dcs_5m_dem")
-    glob_pattern = "*/*-DEM-AHD_56_5m.zip"
+    # input_path = os.path.join(pathlib.Path.home(), "tmp/bushfire/nsw_dcs/nsw_dcs_5m_dem")
+    # glob_pattern = "*/*-DEM-AHD_56_5m.zip"
 
-    output_path = os.path.join(pathlib.Path.home(), "tmp/bushfire/nsw_dcs")
-    temp_output_path = os.path.join(output_path, "tmp")
+    output_path = os.path.join(pathlib.Path.home(), "/data/nsw_dcs")
+    temp_output_path = os.path.join(output_path, "/data/nsw_dcs/tmp")
 
     output_dem_file = os.path.join(output_path, "nsw_dcs_5m_dem.tif")
     output_slope_file = os.path.join(output_path, "nsw_dcs_5m_slope.tif")
 
 
 # how many parallel processes to run
-max_processes = multiprocessing.cpu_count()
+max_processes = int(multiprocessing.cpu_count() / 2)
 
 # set max RAM usage (divide by 4 as there are 4 processes - one per dataset)
 gdal.SetCacheMax(int(ram_to_use / 4) * 1024 * 1024)
@@ -76,11 +76,11 @@ def main():
     logger.info(f"START mosaic and transform images : {full_start_time}")
 
     # list of DEM files to process
-    dem_files = get_image_list()
+    files = get_image_list()
 
-    if len(dem_files) > 0:
+    if len(files) > 0:
         # convert DEM images to slope
-        slope_files = convert_to_slope(dem_files)
+        dem_files, slope_files = convert_to_slope(files)
         logger.info(f"\t - created temp slope files : {datetime.now() - start_time}")
         start_time = datetime.now()
 
@@ -93,59 +93,78 @@ def main():
         mosaic_and_transform(dem_files, output_dem_file)
         logger.info(f"\t - created DEM COG : {datetime.now() - start_time}")
 
-        # remove temp files
-        for file in slope_files:
-            os.remove(file)
+        # # remove temp files
+        # for file in slope_files:
+        #     os.remove(file)
 
     logger.info(f"FINISHED mosaic and transform images : {datetime.now() - full_start_time}")
 
 
 def get_image_list():
-    """ get list of image files to mosaic and transform"""
+    """ Get list of image files to mosaic and transform.
+        Creates URLs with GDAL prefixes to enable downloading and reading of ZIP files directly"""
+
     # file_path = os.path.join(input_path, glob_pattern)
     # files = glob.glob(file_path)
 
     files = list()
 
-    for url in urls:
-        # add image file name to URL so GDAL can read it
-        file_name = os.path.basename(urlparse(url).path).replace(".zip", ".asc")
-        files.append("/".join(["/vsizip//vsicurl", url, file_name]))
+    # get file names and MGA zones from reference file
+    with open("nsw_elevation_index.csv", "r") as f:
+        for line in f.read().splitlines():
+            mga_zone, file_name = line.split(",")
+
+            # add image file name to URL so GDAL can read it
+            url = "/".join(["/vsizip//vsicurl", base_url, mga_zone, file_name + ".zip", file_name + ".asc"])
+            files.append(url)
+
+    # if debugging, only process the first 2 files
+    if debug:
+        files = files[:2]
 
     num_images = len(files)
 
-    # if num_images > 0:
-    logger.info(f"\t - processing {num_images} images")
-    # else:
-    #     logger.warning(f"\t - {file_path} has no images")
+    if num_images > 0:
+        logger.info(f"\t - processing {num_images} images")
+    else:
+        logger.warning(f"\t - nsw_elevation_index.csv has no images")
 
     return files
 
 
-def convert_to_slope(dem_files):
+def convert_to_slope(files):
+    dem_files = list()
     slope_files = list()
 
-    with concurrent.futures.ProcessPoolExecutor(int(max_processes / 2)) as executor:
-        futures = {executor.submit(create_slope_image, input_file): input_file for input_file in dem_files}
+    with concurrent.futures.ProcessPoolExecutor(max_processes) as executor:
+        futures = {executor.submit(create_slope_image, input_file): input_file for input_file in files}
 
         for fut in concurrent.futures.as_completed(futures):
-            output_file = fut.result()
-            slope_files.append(output_file)
+            dem_file, slope_file = fut.result()
+            dem_files.append(dem_file)
+            slope_files.append(slope_file)
 
-            logger.info(f"\t\t - created {output_file}")
+            logger.info(f"\t\t - processed {dem_file}")
 
-    return slope_files
+    return dem_files, slope_files
 
 
 def create_slope_image(input_file):
-    """ convert DEM to slope and output as a single Cloud Optimised GeoTIFF (COG) in GDA94 lat/long """
-    file_name = os.path.basename(input_file).replace(".asc", ".tif").replace("-DEM-", "-gdal_slope-")
-    output_file = os.path.join(temp_output_path, file_name)
+    """ convert DEM GeoTIFF and then to slope and output as a single GeoTIFF """
 
-    gdal.DEMProcessing(output_file, input_file, "slope", alg="Horn",
-                       options="-of GTiff -co COMPRESS=DEFLATE -co NUM_THREADS=ALL_CPUS")
+    dem_file_name = os.path.basename(input_file).replace(".asc", ".tif")
+    dem_file = os.path.join(temp_output_path, dem_file_name)
 
-    return output_file
+    gdal.Translate(dem_file, input_file, format="GTiff",
+              options="-co COMPRESS=NONE -co NUM_THREADS=ALL_CPUS")
+
+    slope_file_name = dem_file_name.replace("-DEM-", "-gdal_slope-")
+    slope_file = os.path.join(temp_output_path, slope_file_name)
+
+    gdal.DEMProcessing(slope_file, dem_file, "slope", alg="Horn",
+                       options="-of GTiff -co COMPRESS=NONE -co NUM_THREADS=ALL_CPUS")
+
+    return dem_file, slope_file
 
 
 def mosaic_and_transform(files, output_file):
