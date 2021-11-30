bd114cdbefa1b5934cf30a3b04247e501a72d54f
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-12-01 10:23:35 +1100

    changed process to download & unzip first

--- data_prep/images/ga_5m/01_mosaic_and_transform_images_in_one_go.py
+++ data_prep/images/ga_5m/01_mosaic_and_transform_images_in_one_go.py
@@ -2,11 +2,14 @@
 import boto3
 import concurrent.futures
 import glob
+import io
 import logging
 import multiprocessing
 import os
 import pathlib
 import platform
+import requests
+import zipfile
 # import validate_cloud_optimized_geotiff
 
 from boto3.s3.transfer import TransferConfig
@@ -55,7 +58,8 @@ else:
     output_slope_file = os.path.join(output_path, "ga_5m_slope.tif")
 
 # how many parallel processes to run
-max_processes = int(multiprocessing.cpu_count() / 2)
+max_processes = 1
+# max_processes = int(multiprocessing.cpu_count() / 2)
 
 # set max RAM usage
 gdal.SetCacheMax(int(ram_to_use) * 1024 * 1024)
@@ -72,24 +76,30 @@ def main():
     logger.info(f"START mosaic and transform images : {full_start_time}")
 
     # list of DEM files to process
-    files = get_image_list()
+    urls = get_image_list()
 
-    if len(files) > 0:
+    if len(urls) > 0:
+        # download and unzip image files
+        dem_files = get_image_files(urls)
+        # dem_files = get_image_list_from_disk()  # only used if process fails after this point
+        logger.info(f"\t - downloaded {len(dem_files)} DEM files : {datetime.now() - start_time}")
+        start_time = datetime.now()
+
         # convert DEM images to slope
-        dem_files, slope_files = convert_to_slope(files)
+        slope_files = convert_to_slope(dem_files)
         # dem_files, slope_files = get_image_list_from_disk()  # only used if process fails after this point
         logger.info(f"\t - created {len(slope_files)} temp slope files : {datetime.now() - start_time}")
         start_time = datetime.now()
 
         # mosaic slope images and transform to GDA94 lat/long
         logger.info(f"\t - processing big slope COG")
-        mosaic_and_transform(slope_files, output_slope_file)
+        mosaic_and_transform(slope_files, "slope", output_slope_file)
         logger.info(f"\t - created slope COG : {datetime.now() - start_time}")
         start_time = datetime.now()
 
         # mosaic DEM images and transform to GDA94 lat/long
         logger.info(f"\t - processing big DEM COG")
-        mosaic_and_transform(dem_files, output_dem_file)
+        mosaic_and_transform(dem_files, "dem", output_dem_file)
         logger.info(f"\t - created DEM COG : {datetime.now() - start_time}")
 
         # # remove temp files
@@ -128,11 +138,11 @@ def get_image_list():
     # get file names and MGA zones from reference file
     with open("ga_dem_urls.txt", "r") as f:
         for file_url in f.read().splitlines():
-            file_name = os.path.basename(file_url).replace(".zip", ".tif")
+            # file_name = os.path.basename(file_url).replace(".zip", ".tif")
 
-            # add image file name to URL so GDAL can read it
-            url = "/".join(["/vsizip//vsicurl", file_url, file_name])
-            files.append(url)
+            # # add image file name to URL so GDAL can read it
+            # url = "/".join(["/vsizip//vsicurl", file_url, file_name])
+            files.append(file_url)
 
     # if debugging, only process the first 2 files
     if debug:
@@ -141,15 +151,63 @@ def get_image_list():
     num_images = len(files)
 
     if num_images > 0:
-        logger.info(f"\t - processing {num_images} images")
+        logger.info(f"Processing {num_images} images")
     else:
         logger.warning(f"\t - nsw_elevation_index.csv has no images")
 
     return files
 
 
-def convert_to_slope(files):
-    dem_files = list()
+def get_image_files(urls):
+    logger.info(f"\t - downloading images")
+
+    image_files = list()
+
+    i = 1
+
+    start_time = datetime.now()
+
+    with concurrent.futures.ProcessPoolExecutor(max_processes) as executor:
+        futures = {executor.submit(download_and_unzip_image, url): url for url in urls}
+
+    for fut in concurrent.futures.as_completed(futures):
+        dem_file = fut.result()
+        if dem_file is not None:
+            image_files.append(dem_file)
+            logger.info(f"\t\t - processed file {i}: {dem_file} : {datetime.now() - start_time}")
+        else:
+            logger.warning(f"\t\t - FAILED to process file {i}: {dem_file} : {datetime.now() - start_time}")
+
+        i += 1
+
+    return image_files
+
+
+def download_and_unzip_image(url):
+    file_name = os.path.basename(url).replace(".zip", ".tif")
+    image_dir = os.path.join(temp_output_path, "dem")
+
+    # download zip file
+    response = requests.get(url)
+
+    # Create a StringIO object, which behaves like a file
+    string_buf = io.BytesIO(response.content)
+    string_buf.seek(0)
+
+    # Create a ZipFile object, instantiated with our file-like StringIO object.
+    # Extract all of the data from that StringIO object into files in the provided output directory.
+    myzip = zipfile.ZipFile(string_buf, 'r', zipfile.ZIP_DEFLATED)
+    myzip.extractall(image_dir)
+    myzip.close()
+    string_buf.close()
+
+    # return unzipped file path
+    return os.path.join(image_dir, file_name)
+
+
+def convert_to_slope(dem_files):
+    logger.info(f"\t - creating slope images")
+
     slope_files = list()
 
     i = 1
@@ -157,34 +215,33 @@ def convert_to_slope(files):
     start_time = datetime.now()
 
     with concurrent.futures.ProcessPoolExecutor(max_processes) as executor:
-        futures = {executor.submit(create_slope_image, input_file): input_file for input_file in files}
+        futures = {executor.submit(create_slope_image, dem_file): dem_file for dem_file in dem_files}
 
         for fut in concurrent.futures.as_completed(futures):
-            dem_file, slope_file = fut.result()
-            if dem_file is not None:
-                dem_files.append(dem_file)
+            slope_file = fut.result()
+            if slope_file is not None:
                 slope_files.append(slope_file)
-                logger.info(f"\t\t - processed file {i}: {dem_file} : {datetime.now() - start_time}")
+                logger.info(f"\t\t - processed file {i}: {slope_file} : {datetime.now() - start_time}")
             else:
-                logger.warning(f"\t\t - FAILED to process file {i}: {dem_file} : {datetime.now() - start_time}")
+                logger.warning(f"\t\t - FAILED to process file {i}: {slope_file} : {datetime.now() - start_time}")
 
             i += 1
 
-    return dem_files, slope_files
+    return slope_files
 
 
-def create_slope_image(input_file):
+def create_slope_image(dem_file):
     """ convert DEM to GeoTIFF and then to slope and output as a single GeoTIFF """
 
     try:
         # convert ASC format input DEM file to TIF -- not required as GA 5m DEMs are already uncompressed GeoTIFFs
-        dem_file_name = os.path.basename(input_file)
-        dem_file = os.path.join(temp_output_path, "dem", dem_file_name)
-
-        gdal_dataset = gdal.Translate(dem_file, input_file,
-                                      options="-of GTiff -a_nodata -3.402823e+38 -co TILED=YES -co COMPRESS=NONE "
-                                              "-co BIGTIFF=YES -co NUM_THREADS=ALL_CPUS")
-        del gdal_dataset
+        dem_file_name = os.path.basename(dem_file)
+        # dem_file = os.path.join(temp_output_path, "dem", dem_file_name)
+        #
+        # gdal_dataset = gdal.Translate(dem_file, input_file,
+        #                               options="-of GTiff -a_nodata -3.402823e+38 -co TILED=YES -co COMPRESS=NONE "
+        #                                       "-co BIGTIFF=YES -co NUM_THREADS=ALL_CPUS")
+        # del gdal_dataset
 
         # convert DEM TIF to slope image
         slope_file_name = dem_file_name.replace(".tif", "-gdal_slope.tif")
@@ -195,26 +252,26 @@ def create_slope_image(input_file):
                                                   "-co BIGTIFF=YES -co NUM_THREADS=ALL_CPUS")
         del gdal_dataset
 
-        return dem_file, slope_file
+        return slope_file
     except:
-        return None, None
+        return None
 
 
-def mosaic_and_transform(files, output_file):
+def mosaic_and_transform(files, image_type, output_file):
     start_time = datetime.now()
 
-    # temp_output_file = os.path.join(temp_output_path, "temp.tif")
+    temp_output_file = os.path.join(temp_output_path, f"temp_{image_type}.tif")
 
     # mosaic all merged files and output as a single GeoTIFF in GDA94 lat/long
-    gdal_dataset = gdal.Warp("/vsimem/temp.tif", files,
+    gdal_dataset = gdal.Warp(temp_output_file, files,
                              options="-of GTiff -overwrite -multi -wm 80% -t_srs EPSG:4283 "
                                      "-co BIGTIFF=YES -co TILED=YES -co COMPRESS=NONE -co NUM_THREADS=ALL_CPUS")
     del gdal_dataset
 
-    logger.info(f"\t \t - created big GeoTIFF : {datetime.now() - start_time}")
+    logger.info(f"\t \t - created big {image_type} GeoTIFF : {datetime.now() - start_time}")
 
     # convert GeoTIFF file to a Cloud Optimised GeoTIFF file (COG)
-    gdal_dataset = gdal.Translate(output_file, "/vsimem/temp.tif",
+    gdal_dataset = gdal.Translate(output_file, temp_output_file,
                                   options="-of COG -co BIGTIFF=YES -co COMPRESS=DEFLATE -co NUM_THREADS=ALL_CPUS")
     del gdal_dataset
 
