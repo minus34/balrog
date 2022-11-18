a8c500687b7a4ac3ae405fa5aa5c6b5626d7d1ba
Hugh Saalmans <hugh.saalmans@iag.com.au>
2022-11-18 17:19:05 +1100

    altering ec2 scripts for personal AWS env

--- data_prep/images/nsw_dcs/01_create_ec2_instance.sh
+++ data_prep/images/nsw_dcs/01_create_ec2_instance.sh
@@ -31,7 +31,7 @@ INSTANCE_TYPE="r5d.16xlarge"
 USER="ec2-user"
 
 # load AWS parameters
-. ${HOME}/.aws/iag_ec2_vars.sh
+. ${HOME}/.aws/minus34/minus34_ec2_vars.sh
 
 # script to check instance status
 PYTHON_SCRIPT="import sys, json
@@ -52,27 +52,29 @@ INSTANCE_ID=$(aws --profile minus34 ec2 run-instances \
 --image-id ${AMI_ID} \
 --count 1 \
 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=hs_testing}]" \
---capacity-reservation-specification none \
+--capacity-reservation-specification '{"CapacityReservationPreference": "none"}' \
 --instance-type ${INSTANCE_TYPE} \
 --key-name ${AWS_KEYPAIR} \
+--security-group-ids ${AWS_SECURITY_GROUP} \
+| \
 python3 -c "import sys, json; print(json.load(sys.stdin)['Instances'][0]['InstanceId'])")
 
 echo "Instance ${INSTANCE_ID} created"
 
 # this doesn't work everytime, hence the while/do below
-aws ec2 wait instance-exists --instance-ids ${INSTANCE_ID}
+aws --profile minus34 ec2 wait instance-exists --instance-ids ${INSTANCE_ID}
 
 # wait for instance to fire up
 INSTANCE_STATE="pending"
 while [ $INSTANCE_STATE != "running" ]; do
     sleep 5
-    INSTANCE_STATE=$(aws ec2 describe-instance-status --instance-id  ${INSTANCE_ID} | python3 -c "${PYTHON_SCRIPT}")
+    INSTANCE_STATE=$(aws --profile minus34 ec2 describe-instance-status --instance-id  ${INSTANCE_ID} | python3 -c "${PYTHON_SCRIPT}")
     echo "  - Instance status : ${INSTANCE_STATE}"
 done
 
-INSTANCE_IP_ADDRESS=$(aws ec2 describe-instances --instance-ids ${INSTANCE_ID} | \
-python3 -c "import sys, json; print(json.load(sys.stdin)['Reservations'][0]['Instances'][0]['PrivateIpAddress'])")
-echo "  - Private IP address : ${INSTANCE_IP_ADDRESS}"
+INSTANCE_IP_ADDRESS=$(aws --profile minus34 ec2 describe-instances --instance-ids ${INSTANCE_ID} | \
+python3 -c "import sys, json; print(json.load(sys.stdin)['Reservations'][0]['Instances'][0]['PublicIpAddress'])")
+echo "  - Public IP address : ${INSTANCE_IP_ADDRESS}"
 
 # save instance details to a local file for easy SSH commands
 echo "export SCRIPT_DIR=${SCRIPT_DIR}" > ~/git/temp_ec2_vars.sh
@@ -80,13 +82,13 @@ echo "export USER=${USER}" >> ~/git/temp_ec2_vars.sh
 echo "export INSTANCE_ID=${INSTANCE_ID}" >> ~/git/temp_ec2_vars.sh
 echo "export INSTANCE_IP_ADDRESS=${INSTANCE_IP_ADDRESS}" >> ~/git/temp_ec2_vars.sh
 
-# wait for SSH to start
+# wait for SSH to start (test it's working by getting permission denied when supplying no key pair)
 INSTANCE_READY=""
 while [ ! $INSTANCE_READY ]; do
     echo "  - Waiting for ready status"
     sleep 5
     set +e
-    OUT=$(ssh -o ConnectTimeout=5 -o StrictHostKeyChecking=no -o BatchMode=yes ${USER}@$INSTANCE_ID 2>&1 | grep "Permission denied" )
+    OUT=$(ssh -o ConnectTimeout=5 -o StrictHostKeyChecking=no -o BatchMode=yes ${USER}@${INSTANCE_IP_ADDRESS} 2>&1 | grep "Permission denied" )
     [[ $? = 0 ]] && INSTANCE_READY='ready'
     set -e
 done
@@ -96,18 +98,18 @@ echo " Copy AWS credentials & supporting files and run remote script"
 echo "-------------------------------------------------------------------------"
 
 # copy AWS creds to access S3
-ssh -F ${SSH_CONFIG} -o StrictHostKeyChecking=no ${INSTANCE_ID} 'mkdir ~/.aws'
-scp -F ${SSH_CONFIG} -r ${HOME}/.aws/credentials ${USER}@${INSTANCE_ID}:~/.aws/credentials
+ssh -i ${AWS_PEM_FILE} -o StrictHostKeyChecking=no ${USER}@${INSTANCE_IP_ADDRESS} 'mkdir ~/.aws'
+scp -i ${AWS_PEM_FILE} -r ${HOME}/.aws/minus34/credentials ${USER}@${INSTANCE_IP_ADDRESS}:~/.aws/credentials
 
 # copy required scripts
-scp -F ${SSH_CONFIG} ${SCRIPT_DIR}/02_remote_setup.sh ${USER}@${INSTANCE_ID}:~/
-scp -F ${SSH_CONFIG} ${SCRIPT_DIR}/03_mosaic_and_transform_images_in_one_go.py ${USER}@${INSTANCE_ID}:~/
+scp -i ${AWS_PEM_FILE} ${SCRIPT_DIR}/02_remote_setup.sh ${USER}@${INSTANCE_IP_ADDRESS}:~/
+scp -i ${AWS_PEM_FILE} ${SCRIPT_DIR}/03_mosaic_and_transform_images_in_one_go.py ${USER}@${INSTANCE_IP_ADDRESS}:~/
 
 # setup proxy (if required) install packages & environment and import data
 if [ -n "${PROXY}" ]; then
-  ssh -F ${SSH_CONFIG} ${USER}@${INSTANCE_ID} ". ./02_remote_setup.sh -p ${PROXY}"
+  ssh -i ${AWS_PEM_FILE} ${USER}@${INSTANCE_IP_ADDRESS} ". ./02_remote_setup.sh -p ${PROXY}"
 else
-  ssh -F ${SSH_CONFIG} ${USER}@${INSTANCE_ID} ". ./02_remote_setup.sh"
+  ssh -i ${AWS_PEM_FILE} ${USER}@${INSTANCE_IP_ADDRESS} ". ./02_remote_setup.sh"
 fi
 
 echo "-------------------------------------------------------------------------"
@@ -116,4 +118,4 @@ echo " End time : $(date)"
 echo " Build took $((duration / 60)) mins"
 echo "----------------------------------------------------------------------------------------------------------------"
 
-ssh -F ${SSH_CONFIG} ${INSTANCE_ID}
+ssh -i ${AWS_PEM_FILE} ${USER}@${INSTANCE_IP_ADDRESS}
