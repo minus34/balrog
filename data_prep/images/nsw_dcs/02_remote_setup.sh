8f2bb09d819091a3253c02b6164cfe13c2ca2073
Hugh Saalmans <hugh.saalmans@iag.com.au>
2022-11-18 15:54:02 +1100

    altering ec2 scripts for personal AWS env

---
+++ data_prep/images/nsw_dcs/02_remote_setup.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+# required to keep long running sessions active
+sudo yum install -y tmux
+
+# check if proxy server required
+while getopts ":p:" opt; do
+  case $opt in
+  p)
+    PROXY=$OPTARG
+    ;;
+  esac
+done
+
+if [ -n "${PROXY}" ];
+  then
+    export no_proxy="localhost,127.0.0.1,:11";
+    export http_proxy="$PROXY";
+    export https_proxy=${http_proxy};
+    export HTTP_PROXY=${http_proxy};
+    export HTTPS_PROXY=${http_proxy};
+    export NO_PROXY=${no_proxy};
+
+    echo "-------------------------------------------------------------------------";
+    echo " Proxy is set to ${http_proxy}";
+    echo "-------------------------------------------------------------------------";
+fi
+
+# Install Conda to create a Python 3.9 environment (AWS yum repos stop at Python 3.7)
+echo "-------------------------------------------------------------------------"
+echo " Installing Conda"
+echo "-------------------------------------------------------------------------"
+
+# download & install silently
+curl -fSsl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+sh Miniconda3-latest-Linux-x86_64.sh -b
+
+# initialise Conda & reload bash environment
+${HOME}/miniconda3/bin/conda init
+source ${HOME}/.bashrc
+
+# update Python packages
+echo "y" | conda update conda
+
+echo "-------------------------------------------------------------------------"
+echo " Installing Python packages"
+echo "-------------------------------------------------------------------------"
+
+echo "y" | conda install -c conda-forge gdal rasterio[s3] rio-cogeo psycopg2 postgis shapely fiona requests boto3
+
+# remove proxy if set
+if [ -n "${PROXY}" ];
+  then
+    unset http_proxy
+    unset HTTP_PROXY
+    unset https_proxy
+    unset HTTPS_PROXY
+    unset no_proxy
+    unset NO_PROXY
+
+    echo "-------------------------------------------------------------------------"
+    echo " Proxy removed"
+    echo "-------------------------------------------------------------------------"
+    echo ""
+fi
+
+echo "-------------------------------------------------------------------------"
+echo " Mount storage"
+echo "-------------------------------------------------------------------------"
+
+sudo mkfs -t xfs /dev/nvme1n1
+sudo mkdir /data
+sudo mount /dev/nvme1n1 /data
+
+sudo chown -R ec2-user:ec2-user /data
+mkdir -p /data/dem/cog
+
+#echo "-------------------------------------------------------------------------"
+#echo " Copy Geoscape data from S3"
+#echo "-------------------------------------------------------------------------"
+#
+#aws s3 sync "s3://bushfire-rasters/geoscape/Surface Cover/" "/data/geoscape/Surface Cover/"
+#aws s3 sync "s3://bushfire-rasters/geoscape/Trees/" "/data/geoscape/Trees/"
